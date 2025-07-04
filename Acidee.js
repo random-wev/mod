663acd38d65d59e02f681ce703a0ac4b4f32d745
random-wev <emrebenimxd5666@gmail.com>
2025-07-04 19:14:35 +0300

    Update Acidee.js

--- Acidee.js
+++ Acidee.js
@@ -1,1 +1,31 @@
+runAfterLoad(function() {
+    elements.hydrofluoric_acid = {
+        color: "#8ff",
+        behavior: behaviors.LIQUID,
+        category: "liquids",
+        state: "liquid",
+        boilingPoint: 200,
+        reactions: {
+            glass: { elem1: null, elem2: "steam" }
+        },
+        tempHigh: 200,
+        stateHigh: "steam"
+    };
 
+    elements.sodium_hydroxide = {
+        color: "#ffffff",
+        behavior: behaviors.POWDER,
+        category: "powders",
+        state: "solid",
+        reactions: {
+            water: { elem1: "sodium_hydroxide_solution", elem2: null }
+        }
+    };
+
+    elements.sodium_hydroxide_solution = {
+        color: "#c0ffff",
+        behavior: behaviors.LIQUID,
+        category: "liquids",
+        state: "liquid"
+    };
+});
